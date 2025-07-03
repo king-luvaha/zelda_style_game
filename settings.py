507b36b638acda07d0c85721b96c5a3db93b342a
Luvaha <luvaha456@gmail.com>
2025-07-03 15:00:30 +0300

    Weapons

--- settings.py
+++ settings.py
@@ -3,3 +3,12 @@ WIDTH    = 1024
 HEIGHT   = 576
 FPS      = 60
 TILESIZE = 64
+
+# weapons
+weapon_data = {
+    'sword': {'cooldown': 100, 'damage': 15, 'graphic': './assets/graphics/weapons/sword/full.png'},
+    'lance': {'cooldown': 400, 'damage': 30, 'graphic': './assets/graphics/weapons/lance/full.png'},
+    'axe': {'cooldown': 300, 'damage': 20, 'graphic': './assets/graphics/weapons/axe/full.png'},
+    'rapier': {'cooldown': 50, 'damage': 8, 'graphic': './assets/graphics/weapons/rapier/full.png'},
+    'sai': {'cooldown': 80, 'damage': 10, 'graphic': './assets/graphics/weapons/sai/full.png'},
+}
