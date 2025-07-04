cf5050af8765102cc4fc2c8339647ce55d9c9aab
Luvaha <luvaha456@gmail.com>
2025-07-04 22:18:23 +0300

    Added Sound to the game

--- main.py
+++ main.py
@@ -12,6 +12,11 @@ class Game:
         self.clock = pygame.time.Clock()
 
         self.level = Level()
+
+        # sound
+        main_sound = pygame.mixer.Sound('./assets/audio/main.ogg')
+        main_sound.set_volume(0.5)
+        main_sound.play(loops= -1)
     
     def run(self):
         while True:
--- player.py
+++ player.py
@@ -51,6 +51,10 @@ class Player(Entity):
         self.hurt_time = None
         self.invulnerability_duration = 500
 
+        # Import a sound
+        self.weapon_attack_sound = pygame.mixer.Sound('./assets/audio/sword.wav')
+        self.weapon_attack_sound.set_volume(0.4)
+
     def import_player_assets(self):
         character_path = './assets/graphics/player/'
         self.animations = {
@@ -91,6 +95,7 @@ class Player(Entity):
                 self.attacking = True
                 self.attack_time = pygame.time.get_ticks()
                 self.create_attack()
+                self.weapon_attack_sound.play()
 
             # Magic Input
             if keys[pygame.K_LCTRL]:
